a41467d732851710f224c643596770763661b5c5
子发 <zifa@headlinetime.cn>
2018-10-14 17:06:27 +0800

    .

--- src/lang/index.js
+++ src/lang/index.js
@@ -2,4 +2,24 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import enLocale from 'element-ui/lib/locale/lang/en'
 import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import zh from './zh'
+import en from './en'
+Vue.use(VueI18n)
 
+const messages = {
+  zh: {
+    ...zh,
+    ...zhLocale
+  },
+  en: {
+    ...en,
+    ...enLocale
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'zh',
+  messages
+})
+
+export default i18n
--- src/main.js
+++ src/main.js
@@ -4,15 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import i18n from './lang'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(Element)
+Vue.use(Element, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
